83da3ff640380503d2bb4ffd099084dec7a3ca14
sinooo2001 <sina098ali@gmail.com>
2023-03-28 13:28:55 +0300

    sina

---
+++ mobil_shop/lib/view/home.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/src/widgets/container.dart';
+import 'package:flutter/src/widgets/framework.dart';
+
+class home extends StatelessWidget {
+  const home({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
